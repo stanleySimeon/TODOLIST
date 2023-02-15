f3303d1c84c3c6323c5bf2257ad2ba95dc5d2376
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-15 04:40:05 -0500

    update dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -28,4 +29,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
 };
